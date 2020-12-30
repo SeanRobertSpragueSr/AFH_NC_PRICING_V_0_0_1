6880f87f6297a50cd0b172908ab9b5994a1760e4
Sean Sprague <seansprageusr@gmail.com>
2020-12-30 13:51:13 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"seanspraguesr@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
