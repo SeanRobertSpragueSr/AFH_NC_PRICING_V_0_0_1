5d9cb8f8bd948ac2583199d97a93e95eb5a98135
Sean Sprague <seansprageusr@gmail.com>
2020-12-30 12:39:02 -0500

    Create PRICING_PAGE.py

---
+++ PRICING_PAGE.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+import pandas as pd
+
+@st.cache
+def load_NC_INV():
+    df=pd.read_csv("https://raw.githubusercontent.com/SeanRobertSpragueSr/AFH_NC_PRICING_V_0_0_1/main/DT_INV.csv")
+    return df
+#"C:\Users\ssprague\Documents\GitHub\NC_PRICING_APP\DT_INV.csv"
+#"C:\Users\ssprague\Documents\GitHub\NC_PRICING_APP\NEW v2 - Master_Copy_Not_For_Distribution.xlsx"
+
+st.title('Current New Car Inventory')
+
+DT_INV=load_NC_INV()
+DT_INV=DT_INV[['Stock Number','Days','Year','Model','VIN','Trim',
+               'Cylinders','Trans','Color','Color Code','MSRP','Invoice','E-Price',
+               'Color DS']]
+
+
+print(DT_INV.head())
+Models=list(DT_INV.Model.unique())
+Models.insert(0,'All_Models')
+
+
+Years=list(DT_INV.Year.unique())
+Years.insert(0,'All_Years')
+
+Trims=list(DT_INV.Trim.unique())
+Trims.insert(0,'All_Trims')
+
+
+
+year=st.sidebar.selectbox('Year',Years)
+model=st.sidebar.selectbox('Models',Models)
+trim=st.sidebar.selectbox('Trims',Trims)
+
+
+
+if year=="All_Years":
+    DDT_INV=DT_INV
+else:
+    DDT_INV=DT_INV[DT_INV['Year']==year]
+
+if model=="All_Models":
+    DDT_INV=DDT_INV
+else:
+    DDT_INV=DDT_INV[DDT_INV['Model']==model]
+    
+if trim =='All_Trims':
+    DDT_INV=DDT_INV
+else:
+    DDT_INV=DDT_INV[DDT_INV['Trim']==trim]
+    
+    
+Colors=list(DDT_INV.Color.unique())
+Colors.insert(0,'All_Colors')
+color=st.sidebar.selectbox('EXT_Colors',Colors)
+
+if color=='All_Colors':
+    DDT_INV=DDT_INV
+else:
+    DDT_INV=DDT_INV[DDT_INV['Color']==color]
+
+    
+st.table(DDT_INV.iloc[:,:])
